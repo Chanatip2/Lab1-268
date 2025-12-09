c438ad7a3a49b542d9613f2485077951b88b33a2
ChanatipRungnoree 6604062610284 <s6604062610284@email.kmutnb.ac.th>
2025-12-09 14:05:28 +0700

    commit1

---
+++ clocks.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    
+	int h, m, s;
+    
+    scanf("%d:%d:%d", &h, &m, &s);
+    
+    printf("Hour:%d\nMinute:%d\nSecond:%d\n", h, m, s);
+    
+    return 0;
+}
+
