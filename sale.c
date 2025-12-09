20be6b87c19f92515fb3fee0832a15fc7533a34f
ChanatipRungnoree 6604062610284 <s6604062610284@email.kmutnb.ac.th>
2025-12-09 14:36:33 +0700

    commit3
    calculate VAT

--- sale.c
+++ sale.c
@@ -2,23 +2,30 @@
 
 int main() {
 	
-	float price1, price2, price3, total;
+	float price1, price2, price3, totalvat;
 	
 	printf("Enter Price1: ");
 	scanf("%f",&price1);
 	float total1 = price1 * 0.95 ;
+	totalvat = total1 * 1.07 ;
 	printf("Total promotionA (5%% discount):%.2f\n" , total1);
+	printf("TotalPromotionA VAT(7%%):%.2f\n",totalvat);
 	
 	printf("Enter Price2: ");
 	scanf("%f",&price2);
 	float total2 = price2 * 0.85 ;
+	totalvat = total2 * 1.07 ;
 	printf("Total promotionB (15%% discount):%.2f\n" , total2);
+	printf("TotalPromotionB VAT(7%%):%.2f\n",totalvat);
 	
 	printf("Enter Price3: ");
 	scanf("%f",&price3);
 	float total3 = price3 * 0.70 ;
+	totalvat = total3 * 1.07 ;
 	printf("Total promotionC (30%% discount):%.2f\n" , total3);
+	printf("TotalPromotionC VAT(7%%):%.2f\n",totalvat);
 	
 	return 0;
 		
 }
+
